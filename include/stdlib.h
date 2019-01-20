4a173ba32cb3b4fbe4cfff9b6a8e9823bd645d60
Brent Cook <busterb@gmail.com>
2019-01-19 22:11:44 -0600

    switch which arc4random bits we need

--- include/stdlib.h
+++ include/stdlib.h
@@ -14,10 +14,7 @@
 
 #ifndef HAVE_ARC4RANDOM_
 uint32_t arc4random(void);
-#endif
-
-#ifndef HAVE_ARC4RANDOM_UNIFORM
-uint32_t arc4random_uniform(uint32_t);
+void arc4random_buf(void *buf, size_t nbytes);
 #endif
 
 #ifndef HAVE_DAEMON
