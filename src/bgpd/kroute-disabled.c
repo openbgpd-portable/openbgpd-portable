77a5d25b363c89ebe058a1255a4871210cb3026a
Claudio Jeker <claudio@openbsd.org>
2019-04-11 15:31:10 +0200

    Implement enough kroute nexthop handling code to fake it.
    At the moment all nexthops are automatically valid (not connected
    but using the nexthop as true nexthop).

--- src/bgpd/kroute-disabled.c
+++ src/bgpd/kroute-disabled.c
@@ -15,14 +15,246 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#include <sys/tree.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "bgpd.h"
 #include "session.h"
 #include "log.h"
 
+struct kroute_node {
+	RB_ENTRY(kroute_node)	 entry;
+	struct kroute		 r;
+	struct kroute_node	*next;
+};
+
+struct kroute6_node {
+	RB_ENTRY(kroute6_node)	 entry;
+	struct kroute6		 r;
+	struct kroute6_node	*next;
+};
+
+struct knexthop_node {
+	RB_ENTRY(knexthop_node)	 entry;
+	struct bgpd_addr	 nexthop;
+	void			*kroute;
+};
+
+struct ktable	 krt;
+
+struct ktable	*ktable_get(u_int);
+
+static inline int
+knexthop_compare(struct knexthop_node *a, struct knexthop_node *b)
+{
+	int	i;
+
+	if (a->nexthop.aid != b->nexthop.aid)
+		return (b->nexthop.aid - a->nexthop.aid);
+
+	switch (a->nexthop.aid) {
+	case AID_INET:
+		if (ntohl(a->nexthop.v4.s_addr) < ntohl(b->nexthop.v4.s_addr))
+			return (-1);
+		if (ntohl(a->nexthop.v4.s_addr) > ntohl(b->nexthop.v4.s_addr))
+			return (1);
+		break;
+	case AID_INET6:
+		for (i = 0; i < 16; i++) {
+			if (a->nexthop.v6.s6_addr[i] < b->nexthop.v6.s6_addr[i])
+				return (-1);
+			if (a->nexthop.v6.s6_addr[i] > b->nexthop.v6.s6_addr[i])
+				return (1);
+		}
+		break;
+	default:
+		fatalx("%s: unknown AF", __func__);
+	}
+
+	return (0);
+}
+
+RB_PROTOTYPE(knexthop_tree, knexthop_node, entry, knexthop_compare)
+RB_GENERATE(knexthop_tree, knexthop_node, entry, knexthop_compare)
+
+#define KT2KNT(x)	(&(ktable_get((x)->nhtableid)->knt))
+
+void	knexthop_send_update(struct knexthop_node *);
+
+static struct knexthop_node *
+knexthop_find(struct ktable *kt, struct bgpd_addr *addr)
+{
+	struct knexthop_node	s;
+
+	bzero(&s, sizeof(s));
+	memcpy(&s.nexthop, addr, sizeof(s.nexthop));
+
+	return (RB_FIND(knexthop_tree, KT2KNT(kt), &s));
+}
+
+static int
+knexthop_insert(struct ktable *kt, struct knexthop_node *kn)
+{
+	if (RB_INSERT(knexthop_tree, KT2KNT(kt), kn) != NULL) {
+		log_warnx("%s: failed for %s", __func__,
+		    log_addr(&kn->nexthop));
+		free(kn);
+		return (-1);
+	}
+
+	knexthop_send_update(kn);
+
+	return (0);
+}
+
+static int
+knexthop_remove(struct ktable *kt, struct knexthop_node *kn)
+{
+	if (RB_REMOVE(knexthop_tree, KT2KNT(kt), kn) == NULL) {
+		log_warnx("%s: failed for %s", __func__,
+		    log_addr(&kn->nexthop));
+		return (-1);
+	}
+
+	free(kn);
+	return (0);
+}
+
+static void
+knexthop_clear(struct ktable *kt)
+{
+	struct knexthop_node	*kn;
+
+	while ((kn = RB_MIN(knexthop_tree, KT2KNT(kt))) != NULL)
+		knexthop_remove(kt, kn);
+}
+
+int
+kr_nexthop_add(u_int rtableid, struct bgpd_addr *addr, struct bgpd_config *conf)
+{
+	struct ktable		*kt;
+	struct knexthop_node	*h;
+
+	if (rtableid == 0)
+		rtableid = conf->default_tableid;
+
+	if ((kt = ktable_get(rtableid)) == NULL) {
+		log_warnx("%s: non-existent rtableid %d", __func__, rtableid);
+		return (0);
+	}
+	if ((h = knexthop_find(kt, addr)) != NULL) {
+		/* should not happen... this is actually an error path */
+		knexthop_send_update(h);
+	} else {
+		if ((h = calloc(1, sizeof(struct knexthop_node))) == NULL) {
+			log_warn("%s", __func__);
+			return (-1);
+		}
+		memcpy(&h->nexthop, addr, sizeof(h->nexthop));
+
+		if (knexthop_insert(kt, h) == -1)
+			return (-1);
+	}
+
+	return (0);
+}
+
+void
+kr_nexthop_delete(u_int rtableid, struct bgpd_addr *addr,
+    struct bgpd_config *conf)
+{
+	struct ktable		*kt;
+	struct knexthop_node	*kn;
+
+	if (rtableid == 0)
+		rtableid = conf->default_tableid;
+
+	if ((kt = ktable_get(rtableid)) == NULL) {
+		log_warnx("%s: non-existent rtableid %d", __func__,
+		    rtableid);
+		return;
+	}
+	if ((kn = knexthop_find(kt, addr)) == NULL)
+		return;
+
+	knexthop_remove(kt, kn);
+}
+
+void
+knexthop_send_update(struct knexthop_node *kn)
+{
+	struct kroute_nexthop	 n;
+#if 0
+	struct kroute_node	*kr;
+	struct kroute6_node	*kr6;
+#endif
+
+	bzero(&n, sizeof(n));
+	memcpy(&n.nexthop, &kn->nexthop, sizeof(n.nexthop));
+
+#if 0
+	if (kn->kroute == NULL) {
+		n.valid = 0;	/* NH is not valid */
+		send_nexthop_update(&n);
+		return;
+	}
+
+	switch (kn->nexthop.aid) {
+	case AID_INET:
+		kr = kn->kroute;
+		n.valid = kroute_validate(&kr->r);
+		n.connected = kr->r.flags & F_CONNECTED;
+		if (kr->r.nexthop.s_addr != 0) {
+			n.gateway.aid = AID_INET;
+			n.gateway.v4.s_addr = kr->r.nexthop.s_addr;
+		}
+		if (n.connected) {
+			n.net.aid = AID_INET;
+			n.net.v4.s_addr = kr->r.prefix.s_addr;
+			n.netlen = kr->r.prefixlen;
+		}
+		break;
+	case AID_INET6:
+		kr6 = kn->kroute;
+		n.valid = kroute6_validate(&kr6->r);
+		n.connected = kr6->r.flags & F_CONNECTED;
+		if (memcmp(&kr6->r.nexthop, &in6addr_any,
+		    sizeof(struct in6_addr)) != 0) {
+			n.gateway.aid = AID_INET6;
+			memcpy(&n.gateway.v6, &kr6->r.nexthop,
+			    sizeof(struct in6_addr));
+		}
+		if (n.connected) {
+			n.net.aid = AID_INET6;
+			memcpy(&n.net.v6, &kr6->r.prefix,
+			    sizeof(struct in6_addr));
+			n.netlen = kr6->r.prefixlen;
+		}
+		break;
+	}
+#else
+	n.valid = 1;		/* NH is always valid */
+	memcpy(&n.gateway, &kn->nexthop, sizeof(n.gateway));
+#endif
+	send_nexthop_update(&n);
+}
+
 int
 kr_init(void)
 {
+	struct ktable	*kt = &krt;;
+
+	/* initialize structure ... */
+	strlcpy(kt->descr, "rdomain_0", sizeof(kt->descr));
+	RB_INIT(&kt->krt);
+	RB_INIT(&kt->krt6);
+	RB_INIT(&kt->knt);
+	TAILQ_INIT(&kt->krn);
+	kt->fib_conf = kt->fib_sync = 0;
+	kt->rtableid = 0;
+	kt->nhtableid = 0;
+
 	/* XXX need to return an FD that can be polled */
 	return (-1);
 }
@@ -30,6 +262,7 @@ kr_init(void)
 void
 kr_shutdown(u_int8_t fib_prio, u_int rdomain)
 {
+	knexthop_clear(&krt);
 }
 
 void
@@ -76,18 +309,6 @@ kr_delete(u_int rtableid, struct kroute_full *kl, u_int8_t fib_prio)
 }
 
 int
-kr_nexthop_add(u_int rtableid, struct bgpd_addr *addr, struct bgpd_config *conf)
-{
-	return (-1);
-}
-
-void
-kr_nexthop_delete(u_int rtableid, struct bgpd_addr *addr,
-    struct bgpd_config *conf)
-{
-}
-
-int
 kr_reload(void)
 {
 	return (0);
@@ -101,10 +322,78 @@ kr_net_reload(u_int rtableid, u_int64_t rd, struct network_head *nh)
 void
 kr_show_route(struct imsg *imsg)
 {
-	u_int code = CTL_RES_DENIED /* XXX */;
+	struct ctl_show_nexthop	 snh;
+	struct ktable		*kt;
+	struct knexthop_node	*h;
+	int			 code;
 
-	send_imsg_session(IMSG_CTL_RESULT, imsg->hdr.pid,
-	    &code, sizeof(code));
+	switch (imsg->hdr.type) {
+	case IMSG_CTL_SHOW_NEXTHOP:
+		kt = ktable_get(imsg->hdr.peerid);
+		if (kt == NULL) {
+			log_warnx("%s: table %u does not exist", __func__,
+			    imsg->hdr.peerid);
+			break;
+		}
+		RB_FOREACH(h, knexthop_tree, KT2KNT(kt)) {
+			bzero(&snh, sizeof(snh));
+			memcpy(&snh.addr, &h->nexthop, sizeof(snh.addr));
+#if 0
+			if (h->kroute != NULL) {
+				switch (h->nexthop.aid) {
+				case AID_INET:
+					kr = h->kroute;
+					snh.valid = kroute_validate(&kr->r);
+					snh.krvalid = 1;
+					memcpy(&snh.kr.kr4, &kr->r,
+					    sizeof(snh.kr.kr4));
+					ifindex = kr->r.ifindex;
+					break;
+				case AID_INET6:
+					kr6 = h->kroute;
+					snh.valid = kroute6_validate(&kr6->r);
+					snh.krvalid = 1;
+					memcpy(&snh.kr.kr6, &kr6->r,
+					    sizeof(snh.kr.kr6));
+					ifindex = kr6->r.ifindex;
+					break;
+				}
+				if ((kif = kif_find(ifindex)) != NULL)
+					memcpy(&snh.iface,
+					    kr_show_interface(&kif->k),
+					    sizeof(snh.iface));
+			}
+#else
+			snh.valid = 1;
+			snh.krvalid = 1;
+#endif
+			send_imsg_session(IMSG_CTL_SHOW_NEXTHOP, imsg->hdr.pid,
+			    &snh, sizeof(snh));
+		}
+		break;
+	case IMSG_CTL_SHOW_FIB_TABLES:
+		{
+			struct ktable	ktab;
+
+			ktab = krt;
+			/* do not leak internal information */
+			RB_INIT(&ktab.krt);
+			RB_INIT(&ktab.krt6);
+			RB_INIT(&ktab.knt);
+			TAILQ_INIT(&ktab.krn);
+
+			send_imsg_session(IMSG_CTL_SHOW_FIB_TABLES,
+			    imsg->hdr.pid, &ktab, sizeof(ktab));
+		}
+		break;
+	default:	/* nada */
+		code = CTL_RES_DENIED /* XXX */;
+		send_imsg_session(IMSG_CTL_RESULT, imsg->hdr.pid,
+		    &code, sizeof(code));
+		return;
+	}
+
+	send_imsg_session(IMSG_CTL_END, imsg->hdr.pid, NULL, 0);
 }
 
 void
@@ -120,6 +409,14 @@ ktable_exists(u_int rtableid, u_int *rdomid)
 		return (1);
 	}
 	return (0);
+}
+
+struct ktable *
+ktable_get(u_int rtableid)
+{
+	if (rtableid == 0)
+		return &krt;
+	return NULL;
 }
 
 int
