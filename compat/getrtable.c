98ae8f89e5b7bfa11eb0d27d1b23bc348d0786ab
Claudio Jeker <claudio@openbsd.org>
2022-06-01 17:24:43 +0200

    Check for availability of setfib(2) to implement a version of getrtable in FreeBSD.

--- compat/getrtable.c
+++ compat/getrtable.c
@@ -5,7 +5,21 @@
 
 #include <sys/socket.h>
 
+#ifdef HAVE_SETFIB
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <stddef.h>
+#endif
+
 int getrtable(void)
 {
+#ifdef HAVE_SETFIB
+	int	fib;
+	size_t	len = sizeof(fib);
+
+	if (sysctlbyname("net.my_fibnum", &fib, &len, NULL, 0) == -1)
+		return -1;
+	return fib;
+#endif
 	return 0;
 }
